c40ea304508c2b55f22e22cc592baafc38ce078d
Zhikun Zhang <zhikun.zhang@rwth-aachen.de>
2022-01-28 00:35:39 +0100

    change testmatrix

--- src/testmatrix.cc
+++ src/testmatrix.cc
@@ -2,10 +2,13 @@
 //using namespace std;
 int main()
 {
-    std::cout<<"Cmake successfully"<<std::endl;
+    //std::cout<<"Cmake successfully"<<std::endl;
     Matrix matrix1,matrix2,matrix;
     std::cin>>matrix1>>matrix2;
     matrix = matrix1 * matrix2;
+    std::cout<<matrix<<std::endl;
+    std::cin>>matrix1>>matrix2;
+    matrix = matrix1 + matrix2;
     std::cout<<matrix<<std::endl;
     return 0;
 }
